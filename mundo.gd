59f3cba75d48cd66c62ec699c6bbd2602a519fab
FernandoMora29 <morafernando2906@gmail.com>
2025-05-19 16:54:09 -0400

    Ahora sí agarró.
    -Colisiones del mundo añadidas

--- mundo.gd
+++ mundo.gd
@@ -1,6 +1,6 @@
 extends Node2D
 @onready var colision_del_mapa: CollisionPolygon2D = $"StaticBody2D/Colision del Mapa"
-@onready var polygon_2d: Polygon2D = $StaticBody2D/Arriba/Polygon2D
+@onready var polygon_2d: Polygon2D = $"StaticBody2D/Colision del Mapa/Polygon2D"
 
 func _ready():
 	polygon_2d.polygon = colision_del_mapa.polygon
